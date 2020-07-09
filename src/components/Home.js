d425226122ccfb30cf8dcdbcbbaafca09ad73a92
Andrew Nicholl <andrewnicholl@Andrews-MBP.lan>
2020-07-09 15:40:29 -0400

    Change portfolio link to dribbble playbook, add Medium link

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,11 +26,10 @@ function Home(props) {
             </div>
             <div>
               <a
-                href="https://www.dropbox.com/s/44rakq0gn6qp6zs/andrewnicholl.pdf?dl=0"
-                target="_blank"
+                href="http://portfolio.nicholldesign.com/"
                 className="button desktop-link"
               >
-                <p className="paragraph">see portfolio</p>
+                <p className="paragraph">see my work</p>
                 <div className="button__arrow">
                   <div className="button__line1"></div>
                   <div className="button__line2"></div>
@@ -38,11 +37,10 @@ function Home(props) {
                 </div>
               </a>
               <a
-                href="https://www.dropbox.com/s/44rakq0gn6qp6zs/andrewnicholl.pdf?dl=0"
-                target="_blank"
+                href="http://portfolio.nicholldesign.com/"
                 className="button mobile-link"
               >
-                <p className="paragraph">see portfolio</p>
+                <p className="paragraph">see my work</p>
                 <div className="button__arrow">
                   <div className="button__line1"></div>
                   <div className="button__line2"></div>
@@ -89,6 +87,16 @@ function Home(props) {
                   </svg>
                 </a>
 
+                <a
+                  href="https://medium.com/@andrewnicholl"
+                  target="_blank"
+                  className="social__link"
+                >
+                  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="social__icon">
+<path d="M26.0844 8.74375L28 6.90312V6.5H21.3656L16.6375 18.325L11.2594 6.5H4.30312V6.90312L6.54063 9.60938C6.75938 9.80938 6.87187 10.1031 6.84375 10.3969V21.0312C6.9125 21.4156 6.79062 21.8063 6.52187 22.0844L4 25.1562V25.5531H11.1438V25.15L8.625 22.0875C8.49005 21.9505 8.38854 21.7842 8.32837 21.6015C8.26821 21.4189 8.25101 21.2248 8.27812 21.0344V11.8344L14.55 25.5594H15.2781L20.6719 11.8344V22.7687C20.6719 23.0562 20.6719 23.1156 20.4844 23.3062L18.5438 25.1906V25.5938H27.9562V25.1906L26.0844 23.35C25.9219 23.225 25.8375 23.0156 25.8719 22.8125V9.28125C25.8557 9.18083 25.8669 9.0779 25.9043 8.98331C25.9417 8.88872 26.0039 8.80596 26.0844 8.74375V8.74375Z" fill="white"/>
+</svg>
+
+                  </a>
                 <a
                   href="https://www.linkedin.com/in/andrew-nicholl/"
                   target="_blank"
